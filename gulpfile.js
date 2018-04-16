2d973bbd131cab0911a1b2b10ec1eb44de9112a5
csheahan <chris.sheahan@live.com>
2018-04-15 19:08:34 -0700

    Add school tab

--- gulpfile.js
+++ gulpfile.js
@@ -51,6 +51,7 @@ gulp.task('nunjucksRender', function () {
   return gulp.src(srcDir + '/pages/**/*.+(njk)')
     .pipe(data(requireJson('work')))
     .pipe(data(requireJson('projects')))
+    .pipe(data(requireJson('classes')))
     .pipe(nunjucksRender({
       path: [srcDir + '/templates',],
       manageEnv: manageEnvironment,
